029cb6bee7768f7740da3d9861440c1341bf286b
zishan344 <zishanahmed344@gmail.com>
2022-02-22 11:54:36 +0600

    done the 4 and 5 number video

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 function loadData() {
   fetch("https://jsonplaceholder.typicode.com/todos/1")
-    .then((Response) => Response.json())
+    .then((res) => res.json())
     .then((data) => console.log(data));
 }
 
@@ -9,18 +9,19 @@ function loadUser() {
     .then((res) => res.json())
     .then((data) => displayUser(data));
 }
+
 function loadPost() {
   fetch("https://jsonplaceholder.typicode.com/posts")
     .then((res) => res.json())
     .then((data) => console.log(data));
 }
-
 function displayUser(data) {
   const ul = document.getElementById("users");
   for (const user of data) {
     const li = document.createElement("li");
-    // li.innerText = user.name + " " + user.email;
-    li.innerText = `name: ${user.name}, email: ${user.email}`;
+    li.innerText = `name: ${user.name}  email: ${user.email}`;
     ul.appendChild(li);
+    // console.log(user.);
   }
+  //   console.log(data);
 }
